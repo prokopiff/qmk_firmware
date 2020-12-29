fd012ddb6ff203ee1c35e6b3c3e091114a1be2b8
Volodymyr Prokopiv <prkopiff@gmail.com>
2020-12-29 18:18:54 +0200

    Fix

--- keyboards/crkbd/keymaps/prokopiff/keymap.c
+++ keyboards/crkbd/keymaps/prokopiff/keymap.c
@@ -144,9 +144,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(\
            KC_GRAVE, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,      KC_EQL,  KC_HOME, KC_UP,     KC_PGUP, _______, KC_LGUI, \
-           _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_DOWN,   KC_RGHT, _______, KC_BSLS, \
-           _______,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      KC_VOLD, KC_END,  KC_PSCR,   KC_PGDN, _______, KC_RCTL,  \
-                                      _______, _______, _______,      _______, _______, _______\
+           _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      _______, KC_LEFT, KC_DOWN,   KC_RGHT, _______, KC_BSLS, \
+           _______,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      _______, KC_END,  KC_PSCR,   KC_PGDN, _______, KC_RCTL,  \
+                                       _______, _______, _______,      _______, _______, _______\
 ),
 
 [_ADJUST] = LAYOUT(\
