75ee32aaf8d85f58b7808f807358adec47953d1f
Volodymyr Prokopiv <prkopiff@gmail.com>
2020-12-30 12:39:28 +0200

    Add numlock, volume control, wpm counter

--- keyboards/crkbd/keymaps/prokopiff/rules.mk
+++ keyboards/crkbd/keymaps/prokopiff/rules.mk
@@ -16,7 +16,8 @@ BLUETOOTH_ENABLE 	= no     # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE 	= yes    # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE 	= no	 # Enable one-hand typing
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE 	= no    # Breathing sleep LED during USB suspend
+WPM_ENABLE 			= yes
 
 BOOTLOADER = caterina
 
