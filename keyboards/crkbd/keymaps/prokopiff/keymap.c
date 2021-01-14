e7e672bcc9546f98d8db7e038f26429e5e86bc86
Volodymyr Prokopiv <prkopiff@gmail.com>
2021-01-14 14:26:47 +0200

    Add ctrl+tab and ctrl+shift+tab

--- keyboards/crkbd/keymaps/prokopiff/keymap.c
+++ keyboards/crkbd/keymaps/prokopiff/keymap.c
@@ -143,9 +143,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_RAISE] = LAYOUT(\
-           KC_GRAVE, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,      KC_EQL,  KC_HOME, KC_UP,     KC_PGUP, _______, KC_LGUI, \
-           _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_VOLU, KC_LEFT, KC_DOWN,   KC_RGHT, _______, KC_BSLS, \
-           KC_NLCK,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      KC_VOLD, KC_END,  KC_PSCR,   KC_PGDN, _______, KC_RCTL,  \
+           KC_GRAVE, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,      KC_EQL,  KC_HOME, KC_UP,     KC_PGUP, _______,      KC_LGUI, \
+           _______,  KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_VOLU, KC_LEFT, KC_DOWN,   KC_RGHT, C(KC_TAB),    KC_BSLS, \
+           KC_NLCK,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      KC_VOLD, KC_END,  KC_PSCR,   KC_PGDN, C(S(KC_TAB)), KC_RCTL,  \
                                        _______, _______, _______,      _______, _______, _______\
 ),
 
